1154d0ffc86bfc0e814057163f69aa0df53ff47d
LAPTOP-FRII67HM\saral <saralilrap@gmail.com>
2022-01-27 23:02:49 +0100

    Created a minigame manager for level 2-1 that spawns slippers at random position in spawning area

--- Assets/Scripts/CollectableSlipper.cs
+++ Assets/Scripts/CollectableSlipper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinigameManager_2_1 : MonoBehaviour
+public class CollectableSlipper : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class MinigameManager_2_1 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name);
     }
 }
---
+++ Assets/Scripts/MinigameManager_2_1.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManager_2_1 : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject spawningArea;
+
+    [SerializeField]
+    private int numSpawnable = 20;
+
+    [SerializeField]
+    private GameObject spawnablePrefab;
+
+    public int score = 0;
+
+    [SerializeField]
+    private int minScoreToWin = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i < numSpawnable; i ++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-spawningArea.transform.localScale.x / 2, spawningArea.transform.localScale.x / 2), Random.Range(-spawningArea.transform.localScale.y / 2, spawningArea.transform.localScale.y/2));
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360)));
+            Instantiate(spawnablePrefab, pos, rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IncreaseScore()
+    {
+        score++;
+        if (score >= minScoreToWin) // if with timer check also timer end
+        {
+            EndMinigame();
+        }
+    }
+
+    void EndMinigame()
+    {
+        // Dialog
+        // NextLevel
+    }
+}
